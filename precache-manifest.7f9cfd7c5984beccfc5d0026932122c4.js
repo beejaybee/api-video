379ad9a915bbbda791f9ca645bb610423e358424
beejaybee <oyewob@gmail.com>
2019-12-13 21:48:01 +0100

    Updates

---
+++ precache-manifest.7f9cfd7c5984beccfc5d0026932122c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a48f86740739fdc7231571c135668f7",
+    "url": "/api-video/index.html"
+  },
+  {
+    "revision": "4549ab64e7c3a3a04c4c",
+    "url": "/api-video/static/css/main.77705476.chunk.css"
+  },
+  {
+    "revision": "807cad4dae8a6946a09b",
+    "url": "/api-video/static/js/2.1af5f73a.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/api-video/static/js/2.1af5f73a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4549ab64e7c3a3a04c4c",
+    "url": "/api-video/static/js/main.b0614fff.chunk.js"
+  },
+  {
+    "revision": "c152627adc34b7192100",
+    "url": "/api-video/static/js/runtime-main.6947e9f4.js"
+  }
+]);
